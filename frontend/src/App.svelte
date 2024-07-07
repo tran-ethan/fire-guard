d7dbae82f82138a0a48c3ce19b1b40baad18ae9a
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-07-07 12:52:33 -0400

    Install flowbite-svelte and make basic navbar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import Map from "./lib/MapBox/Map.svelte";
+  import Navbar from "./lib/Navbar.svelte";
 </script>
 
+<nav>
+  <Navbar/>
+</nav>
 <main>
   <h1>Sample Mapbox-Gl Map</h1>
   <Map />
---
+++ frontend/src/lib/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+
+  import { Navbar, NavBrand, NavHamburger, NavLi, NavUl } from "flowbite-svelte";
+
+</script>
+<Navbar let:NavContainer color="primary" class="flex flex-row">
+    <NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
+      <NavBrand href="/">
+        <img src="/fireguard-logo.webp" class="size-10" alt="Fire Guard Logo" />
+        <span class="self-center whitespace-nowrap text-xl font-semibold">Fire Guard</span>
+      </NavBrand>
+    </NavContainer>
+  </Navbar>
