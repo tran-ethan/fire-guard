2f487e3fc297193275c21849b7bced5febe3ba05
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-08-03 12:58:53 -0400

    Make map bounded to Canada

--- frontend/src/components/Map.svelte
+++ frontend/src/components/Map.svelte
@@ -1,34 +1,42 @@
 <script lang="ts">
-    import mapboxgl from "mapbox-gl";
-    import { onMount } from "svelte";
-    mapboxgl.accessToken =
-        "pk.eyJ1IjoicGFyc2FqYWZhcmlhbiIsImEiOiJjbHhxbDljdzYwY2ZpMmxwbnR1dTQ2YThyIn0.4zJwGh7wfpqCorynBky0DQ";
+  import mapboxgl from "mapbox-gl";
+  import { onMount } from "svelte";
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoicGFyc2FqYWZhcmlhbiIsImEiOiJjbHhxbDljdzYwY2ZpMmxwbnR1dTQ2YThyIn0.4zJwGh7wfpqCorynBky0DQ";
 
-    let map: mapboxgl.Map;
+  let map: mapboxgl.Map;
 
-    onMount(() => {
-        const link = document.createElement("link");
-        link.href = "https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css";
-        link.rel = "stylesheet";
-        document.head.appendChild(link);
+  onMount(() => {
+    const link = document.createElement("link");
+    link.href = "https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
 
-        map = new mapboxgl.Map({
-            container: "map",
-            center: [-85, 59],
-            zoom: 3,
-        });
+    map = new mapboxgl.Map({
+      container: "map",
+      center: [-85, 59],
+      zoom: 3,
     });
+
+    // Define bounds that conform to the `LngLatBoundsLike` object.
+    const bounds = [
+      [-141.00194, 41.68139], // [west, south]
+      [-52.61944, 83.11139], // [east, north]
+    ];
+    // Set the map's max bounds.
+    map.setMaxBounds(bounds as mapboxgl.LngLatBoundsLike);
+  });
 </script>
 
 <div id="map"></div>
 
 <style>
-    #map {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 1;
-    }
+  #map {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
 </style>
