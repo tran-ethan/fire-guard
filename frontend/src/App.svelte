ad66a2be35f4ce036b16f713ff45a47ddd1890c1
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-08-26 16:27:20 -0400

    Try again

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,38 +1,7 @@
 <script lang="ts">
-  // import { getWildFires } from "./lib/wildfires";
-  import { onMount } from "svelte";
   import MapPage from "./pages/MapPage.svelte";
-
-  let currentRoute = "/";
-
-  // onMount(async () => {
-  //   try {
-  //     //const fire = await getWildFires();
-  //     console.log("app");
-  //     //console.log(fire);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // });
-
-  const routes: {
-    [key: string]: any;
-  } = {
-    "/": MapPage,
-  };
-
-  onMount(() => {
-    currentRoute = window.location.pathname;
-    window.onpopstate = () => {
-      currentRoute = window.location.pathname;
-    };
-  });
 </script>
 
 <main>
-  {#if routes[currentRoute]}
-    <svelte:component this={routes[currentRoute]} />
-  {:else}
-    <h1>404 - Page Not Found</h1>
-  {/if}
+  <MapPage />
 </main>
